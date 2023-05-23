c7bffcf981534eddd19e5fa9a6394cfe7c7fa476
Tara <tara.obersteller@student.fh-kiel.de>
2023-05-23 17:28:55 +0200

    Geolocater funktioniert

--- lib/views/building_Plan_Widget.dart
+++ lib/views/building_Plan_Widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(const MyApp());
 
