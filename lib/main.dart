d5a83bac5ff1a1e6ba33e49abbd57a862320527f
Muhanad Al Mustafa <muhanad.mustafa@student.fh-kiel.de>
2023-05-25 19:45:28 +0200

    User bleibt bei Öffnen der App angemeldet,NUR nach erfolgreichen Registrierung und verifizierung

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -10,26 +11,41 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
       name: "FH-Wave", options: DefaultFirebaseOptions.android);
+
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Rufe die Authentifizierungsüberprüfung auf
+    checkAuth();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: checkAuth() == false ? const LoginScreen() : const HomeScreen(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       initialRoute: '/',
       routes: {
-        '/': (context) => const SignUpScreen(),
         '/login': (context) => const LoginScreen(),
         '/home': (context) => const HomeScreen(),
         '/signup': (context) => const SignUpScreen(),
       },
     );
+  }
+}
+
+bool checkAuth() {
+  //Authentifizierungsüberprüfung
+  var user = FirebaseAuth.instance.currentUser;
+
+  if (user != null && user.emailVerified) {
+    return true;
+  } else {
+    return false;
   }
 }
