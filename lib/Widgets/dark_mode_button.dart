4bb109ead8d92319186bde06528d771cfd8d8629
Anton Hamann <anton.hamann@student.fh-kiel.de>
2023-05-28 10:56:33 +0200

    Funktioniert

--- lib/Widgets/dark_mode_button.dart
+++ lib/Widgets/dark_mode_button.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 
-Color Hintergrundfarbe = Colors.white;
+Color hintergrundfarbe = Colors.white;
 
 class DarkMode extends StatefulWidget {
+  final Function(Color) updateBackgroundColor; // Callback-Funktion definieren
+
+  const DarkMode(this.updateBackgroundColor, {Key? key}) : super(key: key);
+
   @override
   DarkModeButton createState() => DarkModeButton();
 }
@@ -12,27 +16,30 @@ class DarkModeButton extends State<DarkMode> {
   Widget build(BuildContext context) {
     return OutlinedButton(
       style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(100.0), // Rounding the Edges
-            ),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(100.0),
           ),
-          minimumSize: MaterialStateProperty.all(const Size(200, 50)),
-          maximumSize: MaterialStateProperty.all(const Size(300, 50)),
-          side: MaterialStateProperty.all(
-              const BorderSide(width: 2, color: Colors.black)),
-          // Border-settings
-          foregroundColor: MaterialStateProperty.all(Colors.black)),
-      onPressed: ChangeBackground,
-      child: Icon(Icons.abc),
+        ),
+        minimumSize: MaterialStateProperty.all(const Size(200, 50)),
+        maximumSize: MaterialStateProperty.all(const Size(300, 50)),
+        side: MaterialStateProperty.all(
+          const BorderSide(width: 2, color: Colors.black),
+        ),
+        foregroundColor: MaterialStateProperty.all(Colors.black),
+      ),
+      onPressed: changeBackground,
+      child: Text(hintergrundfarbe.toString()),
     );
   }
 
-  void ChangeBackground() {
+  void changeBackground() {
     setState(() {
-      Hintergrundfarbe =
-          Hintergrundfarbe == Colors.white ? Color(0xFF696969) : Colors.white;
-      print(Hintergrundfarbe);
+      widget.updateBackgroundColor(
+        hintergrundfarbe == Colors.white
+            ? const Color(0xFF696969)
+            : Colors.white,
+      );
     });
   }
 }
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
 import '../Widgets/dark_mode_button.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  NewHomeScreen createState() => NewHomeScreen();
+}
+
+class NewHomeScreen extends State<HomeScreen> {
+  void updateBackgroundColor(Color color) {
+    setState(() {
+      hintergrundfarbe = color;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: Text(hintergrundfarbe.toString()),
+        backgroundColor:
+            hintergrundfarbe, // Aktualisiere die Hintergrundfarbe der AppBar
       ),
       body: Center(
-        child: DarkMode(),
+        child: DarkMode(updateBackgroundColor), // Übergib die Callback-Funktion
       ),
-      backgroundColor: Hintergrundfarbe,
+      backgroundColor: hintergrundfarbe,
     );
   }
 }
