ed83a9ef36649accd54d38a76e28d296a87b2212
vincent.schwarz <vincent.schwarz@student.fh-kiel.de>
2023-05-30 14:51:39 +0000

    Mensa ist jetzt auf der Quicklinks Seite gelistet

--- lib/views/quick_links_screen.dart
+++ lib/views/quick_links_screen.dart
@@ -19,6 +19,12 @@ Widget quickLinksScreen(BuildContext context) {
       ),
       const SizedBox(height: 10), //Spacing
       QuickLinkButton(
+        title: 'Mensa',
+        icon: Icons.food_bank_outlined,
+        targetURL: 'https://studentenwerk.sh/de/mensen-in-kiel?ort=1&mensa=5#mensaplan',
+      ),
+      const SizedBox(height: 10), //Spacing
+      QuickLinkButton(
         title: 'QIS',
         icon: Icons.inventory_2_outlined,
         targetURL: 'https://qis.fh-kiel.de/qisserver/rds?state=user&type=0',
@@ -59,6 +65,7 @@ Widget quickLinksScreen(BuildContext context) {
         icon: Icons.newspaper_rounded,
         targetURL: 'https://www.fh-kiel.de/fh-intern/',
       ),
+      const SizedBox(height: 60), //Spacing zu Seitenende
       /// Quicklinks hier weiter hinzufügen
     ],
   );
